22122837f33d177bc1e85ede65ec6e80cfa141b0
Felipe Mendes Neves <felipemen74@gmail.com>
2025-07-13 19:20:54 -0300

    style: estiliza a pagina inicial com bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,41 +6,46 @@
         <title>Full Stack Web Developer</title>
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
-            @if (Route::has('login'))
-                <nav class="flex items-center justify-end gap-4">
-                    @auth
-                        <a
-                            href="{{ url('/dashboard') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
-                        >
-                            Dashboard
-                        </a>
-                    @else
-                        <a
-                            href="{{ route('login') }}"
-                            class="btn btn-primary"
-                        >
-                            Login
-                        </a>
-
-                        @if (Route::has('register'))
-                            <a
-                                href="{{ route('register') }}"
-                                class="btn btn-secondary">
-                                Registre-se
-                            </a>
-                        @endif
-                    @endauth
-                </nav>
-            @endif
+    <body class="">
+        <header class="">
+            
         </header>
-        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-        </div>
+        <div class="px-4 py-5 my-5 text-center">
+            <h1 class="display-5 fw-bold">Full Stack Web Developer</h1>
+            <div class="col-lg-6 mx-auto">
+                <p class="lead mb-4">Sistema que auxilia no controle de uma fazenda de bovinos</p>
+                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    @if (Route::has('login'))
+                        @auth
+                            <a
+                                href="{{ url('/dashboard') }}"
+                                class=""
+                            >
+                                Dashboard
+                            </a>
+                        @else
+                            <a
+                                href="{{ route('login') }}"
+                                class="btn btn-primary"
+                            >
+                                Login
+                            </a>
 
+                            @if (Route::has('register'))
+                                <a
+                                    href="{{ route('register') }}"
+                                    class="btn btn-secondary">
+                                    Registre-se
+                                </a>
+                            @endif
+                        @endauth
+              
+                    @endif
+                </div>
+            </div>
+        </div>
         @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block"></div>
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block"></div>
         @endif
     </body>
 </html>
